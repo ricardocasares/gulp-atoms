5399dc297fd98e8a3157192303206d7e22cd1d30
Ricardo Casares <ricardocasares@gmail.com>
2016-11-17 02:22:18 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
-const wfix = 'watch'
-const afix = 'gulp-atom-'
-const keys = Object.keys
-const modules = 'node_modules'
+const fs = require('fs')
+const path = require('path')
 const help = require('gulp-help')
 const merge = require('deepmerge')
 
+const keys = Object.keys
+const wfix = 'watch'
+const afix = 'gulp-atom-'
+const modules = 'node_modules'
+
 module.exports = function (gulp, opts = {}) {
-  const g = help(gulp, opts.help || {})
+  gulp = help(gulp, opts.help || {})
 
-  atoms().forEach(atomize(g, opts))
+  atomsLookup()
+    .forEach(createTasks(gulp, opts))
 
-  return g
+  return gulp
 }
 
 // Finds and requires atoms inside your packages
-function atoms () {
-  const fs = require('fs')
-  const path = require('path')
-  const load = x => require(x)
-  const atoms = x => y => (y.indexOf(x) >= 0)
+function atomsLookup () {
+  const join = path.join
+  const byPrefix = x => y => y.startsWith(x)
+  const packages = join(process.cwd(), modules)
+  const load = atom => require(join(packages, atom))
   const lookup = src => fs
     .readdirSync(src)
     .filter(file => fs
-      .statSync(path.join(src, file))
+      .statSync(join(src, file))
       .isDirectory())
-    .filter(atoms(afix))
+    .filter(byPrefix(afix))
     .map(load)
 
-  return lookup(modules)
+  return lookup(packages)
 }
 
 // Creates gulp tasks
-function atomize (gulp, opts) {
+function createTasks (gulp, opts) {
   return function (atom) {
     const configs = configLookup(atom, opts)
 
     keys(configs)
       .forEach(config => {
-        console.log(configs[config])
         gulp.task(config, atom.help, atom.task(gulp, configs[config]))
 
         if (atom.watch) {
@@ -59,7 +62,9 @@ function configLookup (atom, opts) {
     .filter(byName)
     .reduce(toObject, {})
 
+  const defaultConfig = { [atom.name]: atom.config }
+
   return keys(configs).length
     ? configs
-    : { [atom.name]: atom.config }
+    : defaultConfig
 }
