9356ab330ab2ccf59ec19a8ab36650b15f4cefdd
Ricardo Casares <ricardocasares@gmail.com>
2016-11-18 16:28:33 +0100

    Add tests for createTasks function

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const test = require('ava')
 const atomsLookup = require('./lib/atoms-lookup')
 const configLookup = require('./lib/config-lookup')
+const createTasks = require('./lib/create-tasks')
 
 let atoms
 test.before(t => {
@@ -37,3 +38,27 @@ test('configLookup merges default atom config object', t => {
 test('configLookup given configuration overrides default', t => {
   t.is(cfg['test:foo'].overriden, 'yes please')
 })
+
+test('createTasks create gulp tasks', t => {
+  const mock = new GulpMock()
+  const fn = createTasks(mock, opts)
+  fn({
+    name: 'test',
+    help: 'test',
+    task: () => {},
+    watch: () => {},
+    config: {}
+  })
+
+  t.is(mock.tasks.length, 4)
+  t.is(mock.tasks.toString(), 'test,test:watch,test:foo,test:foo:watch')
+})
+
+class GulpMock {
+  constructor () {
+    this.tasks = []
+  }
+  task (name) {
+    this.tasks.push(name)
+  }
+}
